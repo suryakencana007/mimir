da5cb248128efb2b7e66a4a946b13a162aafc309
Nanang Suryadi <nanang.suryadi@kudo.co.id>
2019-03-05 02:34:18 +0700

    Release The Kraken

--- request/aws.go
+++ request/aws.go
@@ -16,9 +16,9 @@ import (
     "github.com/go-chi/chi"
 )
 
-func HandleEvent(chiAdapter *chiadapter.ChiLambda, handler *chi.Mux) interface{} {
+func HandleEvent(handler *chi.Mux) interface{} {
     return func(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-        chiAdapter = chiadapter.New(handler)
-        return chiAdapter.Proxy(req)
+        chiLambda := chiadapter.New(handler)
+        return chiLambda.Proxy(req)
     }
 }
